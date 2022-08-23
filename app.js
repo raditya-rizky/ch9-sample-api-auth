ef2ef2946175a8473a65aa0e22d756503e0a556a
raditya-rizky <gusrizkydharmaputra123@gmail.com>
2022-08-23 21:16:10 +0800

    first init

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+
+const passport = require("./lib/passport");
+const router = require("./routes");
+const swaggerJSON = require("./docs/openapi.json");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
